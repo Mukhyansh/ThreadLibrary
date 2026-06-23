c8b472b65c0857b579831df8dd5311abe9a5394f
Mukhyansh <mukbhateja@gmail.com>
2026-06-24 01:29:34 +0530

    june24

--- src/uthreads.c
+++ src/uthreads.c
@@ -3,16 +3,16 @@
 void init(); //starts everything
 void thread_wrapper(void*(*start)(void*),void* arg); //kinda like a boilerplate for all the Metrics
 int thread_create(int tid,void*(*function)(void*),void*(arg));
-void thread_join(int tid,tcb* th);
-void thread_yield(int tid);
+int thread_join(int tid,void** value_pointer,queue* ready_q);
+void thread_yield();
 void thread_exit(void* value);
-void thread_terminate();
-void interrupt_handler();
-int fire_timer(int time_slice);
-void schedule();
+void thread_finished(); //I initially named it `thread_terminate` but that was confusing as this requires the finished context.
+void interrupt_handler(int signum);
+int fire_timer(int time_slice); //Will fire a signal every 20ms.
+void schedule(); 
 struct timeval schedule_timestamp;
 void scheduler_roundrobin();
-void find_ready_threads(int thid,tcb** return_value,queue* ready_q);
+void find_ready_thread(int thid,tcb** return_value,queue* ready_q);
 
 tcb* main_thread;
 struct itimerval timer_val; // stores the timestamps for creation,first_run and completion.
@@ -31,9 +31,14 @@ int active_threads=0;
 void* returnables[MAX];
 
 void thread_wrapper(void*(*start)(void*),void* arg){
-    start((void*)arg);
+    void* result=start((void*)arg);
     printf("Thread with the ID %d exiting!\n",running_thread->id);
-    thread_terminate();
+    /*
+    thread exit is not working as i need to pass this to a finished context, it can work in
+    some cases when the user wants the thread to terminate mid process.
+    */
+    //thread_exit(NULL); //what better to pass than NULL
+    returnables[running_thread->id]=result; //this works better as this returns the program to the finished context
 }
 
 int fire_timer(int time_slice){
@@ -75,7 +80,7 @@ int thread_create(int thid,void*(*function)(void*),void*(arg)){
         main_thread->stack=main_thread->context->uc_stack.ss_sp;
         /*
         We are using the program's default'ly (i invented this word)
-        allocated stack for the first initial thread for better 
+        allocated stack for the first thread for better 
         understanding. 
         We can create stack for the initial thread as well using malloc
         and it wouldn't make a difference!
@@ -176,13 +181,12 @@ void init(){
     finished_context->uc_stack.ss_size=8096;
     finished_context->uc_stack.ss_flags=0;
 
-    makecontext(finished_context,(void*)&thread_terminate,0);
+    makecontext(finished_context,(void*)&thread_finished,0);
 }
 
-void thread_terminate(){
+void thread_finished(){
     printf("Exiting with last thread id %d.\n",running_thread->id);
     running_thread->curr=FINISHED;
-    active_threads--;
     schedule();
 }
 
@@ -209,7 +213,7 @@ as we have built a timer which will fire every 20 ms and call interrupt handler.
 */
 
 // Cooperative
-void thread_yield(int tid){ 
+void thread_yield(){ 
     swapcontext(running_thread->context,scheduler_context);
     return;
 }
@@ -225,7 +229,7 @@ void thread_exit(void* value){
         returnables[running_thread->id]=value;
     }
     printf("Exiting thread with the id %d.\n",running_thread->id);
-    worker_yield();
+    thread_yield();
 }
 
 // Gets assigned the scheduled context and calls the required scheduler for the rescheduling of the threads!
@@ -243,7 +247,7 @@ void schedule(){
 		time_ms+=(double)((1000000-schedule_timestamp.tv_usec)+running_thread->finish_time.tv_usec) / 1000;
 	}
     running_thread->time_taken+=time_ms;
-    scheduler_roundrobin(running_thread->context);
+    scheduler_roundrobin();
 }
 
 //Stores the same id threads in return_value.
@@ -258,7 +262,7 @@ void find_ready_thread(int thid,tcb** return_value,queue* ready_queue){
     }
 }
 
-void schedule(){
+void scheduler_roundrobin(){
     tcb* next_thread=NULL;
     tcb* prev_thread=running_thread;
 
@@ -277,6 +281,29 @@ void schedule(){
     }
     next_thread->curr=RUNNING;
     running_thread=next_thread;
-    init_timer(TIME_SLICE);
+    fire_timer(TIME_SLICE);
     setcontext(next_thread->context);
+} 
+
+int thread_join(int tid,void** value_pointer,queue* ready_q){
+    tcb* waiting_t=NULL;
+    find_ready_thread(tid,&waiting_t,ready_q);
+    if(waiting_t==NULL){
+        if(tid<=thread_count){
+            if(value_pointer!=NULL){
+                *value_pointer=returnables[tid];
+            }
+            return 0;
+        }
+        return -1;
+    }
+    if(waiting_t->waiting_thread>0){
+        printf("The thread %d has already been joined by another thread!",tid);
+        return -1;
+    }
+    waiting_t->waiting_thread=running_thread->id;
+    running_thread->curr=BLOCKED;
+    running_thread->waiting_for=waiting_t->id;
+    enqueue(waiting_q,running_thread);
+    thread_yield();
 }
